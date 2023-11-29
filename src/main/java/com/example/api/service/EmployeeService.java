690226df3e2bd27dfff4c4aa7cc0a0745dc8ec73
abeeb ahmad <mdahlat94@gmail.com>
2023-11-29 14:37:26 +0100

    update email

--- src/main/java/com/example/api/service/EmployeeService.java
+++ src/main/java/com/example/api/service/EmployeeService.java
@@ -1,10 +1,11 @@
 package com.example.api.service;
 
 import com.example.api.model.data.Employees;
+import com.example.api.model.dto.EmployeeDto;
 import com.example.api.model.dto.request.UpdateEmailDto;
 
 public interface EmployeeService {
     Boolean confirmEmployee(String employeePin);
     Employees findEmployee(String pin);
-    Employees updateEmail(UpdateEmailDto updateEmailDto);
+    EmployeeDto updateEmail(UpdateEmailDto updateEmailDto);
 }
