87289487b8886f5c8995564f78b9db2ee1426fc7
Chijindu Alozie <chijindu.alozie@crusaderpensions.com>
2023-11-29 03:05:18 -0800

    new changes to api for email

---
+++ src/main/java/com/example/api/utils/ConstantUtils.java
@@ -0,0 +1,8 @@
+package com.example.api.utils;
+
+public class ConstantUtils {
+    public static final String EMAIL_ALREADY_EXIST = "Email already exist";
+    public static final String USER_DOES_NOT_EXIST = "User does not exist";
+    public static final String INVALID_PIN = "Invalid pin";
+    public static final String INVALID_EMAIL = "Invalid email";
+}
